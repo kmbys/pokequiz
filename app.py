24bdd9e733a2f86d721cb5e19b7ed0f9fd366403
nsbt <55523468+nsbt@users.noreply.github.com>
2021-03-05 09:56:12 +0900

    Create template

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'hello, world'
+    return render_template('index.j2', message="Jinja2テスト")
 
 if __name__ == '__main__':
     app.run()
