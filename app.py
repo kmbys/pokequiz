b90113bea9627984d9c1a4a5c0a2a9df24d0c2dc
nsbt <55523468+nsbt@users.noreply.github.com>
2021-03-05 10:10:59 +0900

    Update template to add question and options

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.j2', message="Jinja2テスト")
+    return render_template('index.j2', message="1問目")
 
 if __name__ == '__main__':
     app.run()
