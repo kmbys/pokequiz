7971c9bd0bb4c83f5d049b0c380d04c7779711de
nsbt <55523468+nsbt@users.noreply.github.com>
2021-03-05 11:07:39 +0900

    Update question to variable

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ def index():
     return render_template(
         'index.j2',
         message="第1問",
+        question="うまれたときから　せなかに　しょくぶつの　タネが　あって　すこしずつ　おおきく　そだつ。　（『ポケモン ソード』より）",
         options=["フシギダネ", "ヒトカゲ", "ゼニガメ", "ピカチュウ"],
         answer_name="フシギダネ",
         answer_image="https://zukan.pokemon.co.jp/zukan-api/up/images/index/7b705082db2e24dd4ba25166dac84e0a.png"
