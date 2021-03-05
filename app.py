2e48132f9aa3696b4794fde66a75b0553da084e2
nsbt <55523468+nsbt@users.noreply.github.com>
2021-03-05 19:12:46 +0900

    Update Quiz to be created from Pokemon list

--- app.py
+++ app.py
@@ -5,28 +5,38 @@ app = Flask(__name__)
 
 class Pokemon:
     def __init__(self, name, description, image_uri):
-        self._name = name
-        self._description = description
-        self._image_uri = image_uri
+        self.name = name
+        self.description = description
+        self.image_uri = image_uri
 
 class Quiz:
-    def __init__(self):
-        self.question = "うまれたときから　せなかに　しょくぶつの　タネが　あって　すこしずつ　おおきく　そだつ。　（『ポケモン ソード』より）"
+    def __init__(self, pokemons):        
+        self.question = pokemons[0].description
         self.options = [
-            {"name": "フシギダネ", "class_name": "correct_option"},
-            {"name": "ゼニガメ", "class_name": "wrong_option"},
-            {"name": "ヒトカゲ", "class_name": "wrong_option"},
-            {"name": "ピカチュウ", "class_name": "wrong_option"}
+            {"name": pokemons[0].name, "class_name": "correct_option"},
+            {"name": pokemons[1].name, "class_name": "wrong_option"},
+            {"name": pokemons[2].name, "class_name": "wrong_option"},
+            {"name": pokemons[3].name, "class_name": "wrong_option"}
         ]
-        self.answer_name = "フシギダネ"
-        self.answer_image = "https://zukan.pokemon.co.jp/zukan-api/up/images/index/7b705082db2e24dd4ba25166dac84e0a.png"
+        self.answer_name = pokemons[0].name
+        self.answer_image = pokemons[0].image_uri
 
 @app.route('/')
 def index():
+    quiz = Quiz([
+        Pokemon(
+            "フシギダネ",
+            "うまれたときから　せなかに　しょくぶつの　タネが　あって　すこしずつ　おおきく　そだつ。　（『ポケモン ソード』より）",
+            "https://zukan.pokemon.co.jp/zukan-api/up/images/index/7b705082db2e24dd4ba25166dac84e0a.png"
+        ),
+        Pokemon("ゼニガメ", "図鑑1", "https://example.com/image1.png"),
+        Pokemon("ヒトカゲ", "図鑑2", "https://example.com/image2.png"),
+        Pokemon("ピカチュウ", "図鑑3", "https://example.com/image3.png")
+    ])
     return render_template(
         'index.j2',
         message="第1問",
-        quiz=Quiz()
+        quiz=quiz
     )
 
 if __name__ == '__main__':
