58c8aa5b2a2604127bbcea96e98be1becb2b9fc9
nsbt <55523468+nsbt@users.noreply.github.com>
2021-03-06 12:02:31 +0900

    Update query param to be escaped

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, escape
 from lib import Quiz
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    stage = request.args.get("stage")
+    stage = escape(request.args.get("stage"))
     return render_template(
         'index.j2',
         stage=1 if stage is None else stage,
