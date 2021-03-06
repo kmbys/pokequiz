3b2ef57b9fb0b58b5fe9c83c07f607f48a98b20d
nsbt <55523468+nsbt@users.noreply.github.com>
2021-03-06 11:57:00 +0900

    Update app to handle query param stage

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 # -*- coding: utf-8 -*-
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from lib import Quiz
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+    stage = request.args.get("stage")
     return render_template(
         'index.j2',
-        stage=1,
+        stage=1 if stage is None else stage,
         quiz=Quiz(151, 4)
     )
 
