0900fb6782e12ee5c40e90b003497758b396d219
nsbt <55523468+nsbt@users.noreply.github.com>
2021-03-07 17:02:55 +0900

    Update view to show level

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from flask import Flask, redirect, url_for, render_template, request, escape
-from lib import Stage, Quiz
+from lib import Stage, Level, Quiz
 
 app = Flask(__name__)
 
@@ -10,14 +10,17 @@ def index():
 
 @app.route('/levels/<int:level>/stages/<int:stage>')
 def get_quiz(level, stage):
+    level_obj = None
     stage_obj = None
     try:
+        level_obj = Level.from_key(level)
         stage_obj = Stage(stage)
     except ValueError:
         return "予期しないリクエストを受けました", 400
 
     return render_template(
         'index.j2',
+        level=level_obj,
         stage=stage_obj,
         quiz=Quiz(151, 4)
     )
